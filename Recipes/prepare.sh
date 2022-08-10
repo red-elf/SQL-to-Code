045463342cc9a0a1270269ef8db9fed99b4c502c
Milos Vasic <milos85vasic@gmail.com>
2022-08-10 16:57:41 +0300

    Recipes implemented.

--- Recipes/prepare.sh
+++ Recipes/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "To be implemented!"
-exit 1
+echo "Preparing the 'ddl2ode' utility for the installation" && \
+  git submodule init && git submodule update && \
+  echo "The 'ddl2code' utility is prepared for the installation"
