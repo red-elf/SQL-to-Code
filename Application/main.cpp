5b6936586e2c753bb1521ada849ebe7206dbad3f
Milos Vasic <milos85vasic@gmail.com>
2022-08-13 15:08:37 +0300

    Parsing the arguments.

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,12 +1,35 @@
 #include <argparse/argparse.hpp>
 
 #include "Utils.h"
+#include "BuildConfig.h"
+#include "VersionInfo.h"
 
 using namespace Utils;
 
-int main() {
+int main(int argc, char *argv[]) {
 
+    auto tag = "main";
 
-    v("main", "Hello!");
+    argparse::ArgumentParser program(VERSIONABLE_NAME, getVersion());
+
+    program.add_argument("-i", "--input")
+            .required()
+            .help("The path for the input DDL file");
+
+    std::string epilog("Project homepage: ");
+    epilog.append(getHomepage());
+
+    program.add_description(getDescription());
+    program.add_epilog(epilog);
+
+    try {
+
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err) {
+
+        e(tag, err.what());
+        std::exit(1);
+    }
     return 0;
 }
