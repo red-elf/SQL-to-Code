62353938df5e1f087af35896ddc881810c89da3a
Milos Vasic <milos85vasic@gmail.com>
2022-08-17 20:53:08 +0300

    Split into the definition and the implementation.

--- Application/Utils.cpp
+++ Application/Utils.cpp
@@ -2,3 +2,33 @@
 // Created by milosvasic on 13.08.22.
 //
 #include "Utils.h"
+
+std::string Utils::getDataType(DataType &dataType) {
+
+    switch (dataType) {
+        case DataType::BOOLEAN:
+            return "Boolean";
+        case DataType::CHAR:
+            return "Char";
+        case DataType::DATE:
+        case DataType::TIME:
+        case DataType::DATETIME:
+        case DataType::BIGINT:
+        case DataType::LONG:
+            return "Long";
+        case DataType::FLOAT:
+            return "Float";
+        case DataType::DOUBLE:
+        case DataType::REAL:
+        case DataType::DECIMAL:
+            return "Double";
+        case DataType::INT:
+        case DataType::SMALLINT:
+            return "Integer";
+        case DataType::TEXT:
+        case DataType::VARCHAR:
+            return "String";
+        default:
+            throw std::invalid_argument("Unable to determine the data type");
+    }
+}
--- Application/Utils.h
+++ Application/Utils.h
@@ -69,35 +69,7 @@ namespace Utils {
         log(tag, message, level);
     }
 
-    std::string getDataType(DataType &dataType) {
-
-        switch (dataType) {
-            case DataType::BOOLEAN:
-                return "Boolean";
-            case DataType::CHAR:
-                return "Char";
-            case DataType::DATE:
-            case DataType::TIME:
-            case DataType::DATETIME:
-            case DataType::BIGINT:
-            case DataType::LONG:
-                return "Long";
-            case DataType::FLOAT:
-                return "Float";
-            case DataType::DOUBLE:
-            case DataType::REAL:
-            case DataType::DECIMAL:
-                return "Double";
-            case DataType::INT:
-            case DataType::SMALLINT:
-                return "Integer";
-            case DataType::TEXT:
-            case DataType::VARCHAR:
-                return "String";
-            default:
-                throw std::invalid_argument("Unable to determine the data type");
-        }
-    }
+    std::string getDataType(DataType &dataType);
 }
 
 #endif //_UTILS_H
