64bc1346ca6fc568578f50eb29ff05f7a272c3d4
Milos Vasic <milos85vasic@gmail.com>
2022-08-29 17:02:43 +0300

    Scoping the generator - The first touches.

---
+++ Application/generator/IGenerator.h
@@ -0,0 +1,17 @@
+//
+// Created by milosvasic on 29.08.22.
+//
+
+#ifndef _IGENERATOR_H
+#define _IGENERATOR_H
+
+
+class IGenerator {
+
+public:
+
+
+};
+
+
+#endif //_IGENERATOR_H
---
+++ Application/generator/IRecipe.h
@@ -0,0 +1,14 @@
+//
+// Created by milosvasic on 29.08.22.
+//
+
+#ifndef _IRECIPE_H
+#define _IRECIPE_H
+
+
+class IRecipe {
+
+};
+
+
+#endif //_IRECIPE_H
