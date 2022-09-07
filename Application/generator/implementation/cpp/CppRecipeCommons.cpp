c3a3b98407b46a2fd92fd3644ad3a836e4f81ba7
Milos Vasic <milos85vasic@gmail.com>
2022-09-07 17:54:01 +0300

    Using the Commons methods.

--- Application/generator/implementation/cpp/CppRecipeCommons.cpp
+++ Application/generator/implementation/cpp/CppRecipeCommons.cpp
@@ -6,41 +6,9 @@
 
 #include "string"
 #include "algorithm"
+#include "Commons.h"
 
-std::string capitalize(std::string &what){
-
-    what[0] = toupper(what[0]);
-    return what;
-}
-
-std::string deCapitalize(std::string &what){
-
-    what[0] = tolower(what[0]);
-    return what;
-}
-
-std::string camelcase(std::string &what) {
-
-    auto n = what.length();
-
-    int res_ind = 0;
-
-    for (int i = 0; i < n; i++) {
-
-        // check for spaces in the sentence
-        if (what[i] == ' ') {
-
-            what[i + 1] = toupper(what[i + 1]);
-            continue;
-
-        } else {
-
-            what[res_ind++] = what[i];
-        }
-    }
-
-    return what.substr(0, res_ind);
-}
+using namespace Commons::Strings;
 
 std::string CppRecipeCommons::getClassName(std::shared_ptr<ClassNameIngredient> &ingredient) {
 
