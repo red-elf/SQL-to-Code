c01841ede750a31f344610770a73cc4dbcfec173
Milos Vasic <i@mvasic.ru>
2022-09-21 15:09:43 +0300

    Script updated.

--- update_software_toolkit.sh
+++ update_software_toolkit.sh
@@ -4,6 +4,7 @@ HERE="$(pwd)"
 DIR_DEPENDABLE="Dependable"
 DIR_VERSIONABLE="Versionable"
 DIR_INSTALLABLE="Installable"
+DIR_UPSTREAMABLE="Upstreamable"
 
 function UPDATE_MODULE {
 
@@ -17,7 +18,17 @@ function UPDATE_MODULE {
 
   if test -e "$DIR"; then
 
-    if cd "$DIR" && git checkout main && git fetch && git pull && cd "$HERE"; then
+    if cd "$DIR" && git checkout main && git fetch && git pull; then
+
+      if test -e "Upstreams"; then
+
+        echo "We are going to update from the upstreams"
+        if ! git fetch upstream && git pull upstream main; then
+
+          echo "ERROR: Failed to fetch and pull the upstreams"
+          exit 1
+        fi
+      fi
 
       cd "$HERE" &&
         echo "The Software Toolkit module updated: '$DIR'"
@@ -35,3 +46,4 @@ function UPDATE_MODULE {
 UPDATE_MODULE "$DIR_DEPENDABLE"
 UPDATE_MODULE "$DIR_VERSIONABLE"
 UPDATE_MODULE "$DIR_INSTALLABLE"
+UPDATE_MODULE "$DIR_UPSTREAMABLE"
