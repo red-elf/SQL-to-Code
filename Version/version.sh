a18216b4497100ef9462dc1515f92f526349c773
Milos Vasic <i@mvasic.ru>
2022-10-10 15:57:20 +0300

    Version increased to 0.0.3.

--- Version/version.sh
+++ Version/version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export VERSIONABLE_SNAPSHOT=false
+export VERSIONABLE_SNAPSHOT=true
 export VERSIONABLE_NAME="sql2code"
 export VERSIONABLE_VERSION_PRIMARY="0"
 export VERSIONABLE_VERSION_SECONDARY="0"
-export VERSIONABLE_VERSION_PATCH="2"
+export VERSIONABLE_VERSION_PATCH="3"
 export VERSIONABLE_HOMEPAGE="https://github.com/red-elf/SQL-to-Code"
 export VERSIONABLE_DESCRIPTION="Tool for generating the source code from the raw sql."
